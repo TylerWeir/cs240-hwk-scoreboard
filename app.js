fcda52d721b3bca7228b7b813626a5f224fb0e2a
Tyler Weir <tylerw@live.com>
2021-10-13 19:50:37 -1000

    Added the updateRuns method.

--- app.js
+++ app.js
@@ -15,12 +15,35 @@ function fillInning(team, inning, runs) {
     cells[cell_index].innerHTML = `<div class='inning'><p>${runs}</p></div>`;
 }
 
+/**
+ * Fills the run total with the specified number of runs.  
+ * @param {string} team Options are home or away.
+ * @param {number} runs The number of runs the team scored. 
+ */
+function fillRuns(team, runs) {
+    let cells = document.querySelectorAll('td');
+    let cell_index = inningToCellIndex(team, 8);
+    cells[cell_index].innerHTML = `<div class='inning last'><p>${runs}</p></div>`;
+}
+
 
 /**
  *  Updates each team's run cell with the total number of runs scored by the team. 
  */
 function updateRuns(){
-    
+    // First update the home team
+    var runSum = 0;
+    for (let i = 1; i < 8; i++) {
+        runSum += getInningValue('home', i);
+    }
+    fillRuns('home', runSum);
+
+    // First update the home team
+    var runSum = 0;
+    for (let i = 1; i < 8; i++) {
+        runSum += getInningValue('away', i);
+    }
+    fillRuns('away', runSum);
 }
 
 /**
@@ -33,7 +56,7 @@ function getInningValue(team, inning) {
     // Get the inning containers
     let cells = document.querySelectorAll('td');
     let cell_index = inningToCellIndex(team, inning);
-
+    
     // Parse the inner html for the number of funs.
     let innerHTML = cells[cell_index].innerHTML
     
@@ -45,7 +68,12 @@ function getInningValue(team, inning) {
     return 0;
 }
 
-
+/**
+ * Returns the index of the cell that represents the inning. 
+ * @param {number} team Options are home or away.
+ * @param {number} inning Inning number 1-7 inclusive.
+ * @returns {number} The index of the cell the represents the inning. 
+ */
 function inningToCellIndex(team, inning) {
     // Go the right container
     if(team=='home') {
@@ -53,6 +81,4 @@ function inningToCellIndex(team, inning) {
     } else {
         return 9 + inning;
     }
-
-
 }
